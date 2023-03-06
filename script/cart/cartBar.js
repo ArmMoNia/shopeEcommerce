c4c80007c6a1d8f47d170784a1f1ea2a7ad57245
aarm <narupanarts@gmial.com>
2023-03-06 20:55:15 +0700

    add logic delete item from cart

--- script/cart/cartBar.js
+++ script/cart/cartBar.js
@@ -1,16 +1,22 @@
 // display cart bar
-let totalPrice = 0;
 
 const addItemToCartBar = function (addItem) {
+  let totalPrice = 0;
   const totalPriceDisplay = document.querySelector(".cart-bar-total-price");
   const checkOutBtn = document.querySelector(".cart-bar-checkout");
-
-  addItem.map(() => {});
-  console.log(addItem);
-  document.querySelector(".cart-bar-item-list").innerHTML = addItem
-    .map((item) => {
-      totalPrice = totalPrice + item.price * item.qty;
-      return `<div class="cart-bar-my-item">
+  const cartItemList = document.querySelector(".cart-bar-item-list");
+  // add logic for init
+  if (addItem.length === 0) {
+    // render if have no item that user add to cart
+    totalPriceDisplay.classList.add("hidden");
+    checkOutBtn.classList.add("hidden");
+    cartItemList.innerHTML = `<div class="cart-bar-no-item">No Item, Let's shop some cloths!!!</div>`;
+  } else {
+    // render item that user add to cart
+    cartItemList.innerHTML = addItem
+      .map((item) => {
+        totalPrice = totalPrice + item.price * item.qty;
+        return `<div class="cart-bar-my-item">
       <div class="cart-bar-my-item-img">
         <img class="cart-bar-my-item-display" src="${item.image}"  />
       </div>
@@ -21,15 +27,25 @@ const addItemToCartBar = function (addItem) {
           <p class="cart-bar-my-item-qty">Qty: ${item.qty}</p>
           <p class="cart-bar-my-item-price">$ ${item.price * item.qty} </p>
       </div>
+      <button class="delItem">Del</button>
       </div>
       `;
-    })
-    .join("");
-  console.log(totalPrice);
-
-  totalPriceDisplay.classList.remove("hidden");
-  checkOutBtn.classList.remove("hidden");
-  totalPriceDisplay.innerHTML = `Total: $ ${totalPrice.toLocaleString()}`;
+      })
+      .join("");
+    totalPriceDisplay.classList.remove("hidden");
+    checkOutBtn.classList.remove("hidden");
+    totalPriceDisplay.innerHTML = `Total: $ ${totalPrice.toLocaleString()}`;
+  }
+  // add delItem logic
+  if (addItem.length > 0) {
+    const delItem = document.querySelectorAll(".delItem");
+    delItem.forEach((del, index) => {
+      del.addEventListener("click", () => {
+        addItem.splice(index, 1);
+        addItemToCartBar(addItem);
+      });
+    });
+  }
 };
 
 export default addItemToCartBar;
